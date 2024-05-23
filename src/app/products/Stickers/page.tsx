48674204f7087f0638b29b2e320080725b77df8a
Samuel3217 <deriva335@hotmail.com>
2024-05-23 13:10:22 -0600

    fix stickers

--- src/app/products/Stickers/page.tsx
+++ src/app/products/Stickers/page.tsx
@@ -1,18 +1,12 @@
-'use client';
-import React from 'react'
-import { useRouter } from 'next/navigation';
-
+import Barra from "@/app/bar";
 
 function page() {
-    const router = useRouter();
-
-    const handleChangeImage = () => {
-        router.push('/');
-    };
 
   return (
     <div className="">
-        <img src="/images/valore.png" className="w-24 h-15" onClick={handleChangeImage} />
+
+      <Barra/>
+
       <h1 className="text-center text-5xl">Stickers</h1>
       <p className="columns-4 mt-10">
         <img src="/images/sticker.png" className="w-80"></img>

708ffbb9f53aebb7dccc986320f5fd30d4f23cb8
Samuel3217 <deriva335@hotmail.com>
2024-06-19 11:49:46 -0600

    system light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils"
-
+import { ThemeProvider } from "@/components/theme-provider"
 
 
 const fontSans = FontSans({
@@ -28,7 +28,14 @@ export default function RootLayout({
           fontSans.variable 
       )}
           >
-
+            <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+>
+            {children}
+            </ThemeProvider>
             </body>
     </html>
   );

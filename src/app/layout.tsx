6366317620aa9725309114dde4335bfc01158442
Samuel3217 <deriva335@hotmail.com>
2024-06-19 11:45:35 -0600

    body white1.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils"
-import { ThemeProvider } from "@/components/theme-provider"
+
 
 
 const fontSans = FontSans({
@@ -28,14 +28,7 @@ export default function RootLayout({
           fontSans.variable 
       )}
           >
-            <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
->
-            {children}
-            </ThemeProvider>
+
             </body>
     </html>
   );

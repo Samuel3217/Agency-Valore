13e87042a8d9f1c0a69e005eaf29d1e817326e8c
Samuel3217 <deriva335@hotmail.com>
2024-07-23 12:22:44 -0600

    Ofertas page added

---
+++ src/app/Ofertas/page.tsx
@@ -0,0 +1,28 @@
+import Barra from "../bar"
+import { Menu2 } from "../Menu2"
+import Footer from "../footer";
+import BarNew from "../BarNew"
+import BarOfertas from "../BarOfertas";
+
+function page() {
+  return (
+    <div>
+    <div className="mt-2 ml-2 w-[1325px]">
+      <Barra/>
+      </div>
+
+      <h1 className="text-center mt-5 text-3xl text-black hover:text-cyan-700 transition-all duration-300">Nuevos</h1>
+
+      <div className="mt-10 ml-[-10px] w-[1350px]">
+            <BarOfertas/>
+        </div>
+
+        <Menu2/>
+
+        <Footer/>
+
+      </div>
+  )
+}
+
+export default page

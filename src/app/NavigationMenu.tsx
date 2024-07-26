08c9d220bd3cd7c8299b2fbf210b023ab4408b14
Samuel3217 <deriva335@hotmail.com>
2024-07-26 14:04:12 -0600

    update gaby 1.1

--- src/app/NavigationMenu.tsx
+++ src/app/NavigationMenu.tsx
@@ -14,80 +14,70 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
  
-  const components: { title: string; href: string; description: string }[] = [
-    {
-      title: "Imprenta",
-      href: "/servicios/imprenta",
-      description:
-        "Opciónes que van desde Gran formato a impresión y diseño.",
-    },
-    {
-      title: "Branding Corporativo",
-      href: "/servicios/branding",
-      description:
-        "Alcanza tus metas con nosotros, mejora tu imagen publicitaria.",
-    },
-    {
-      title: "Marketing digital",
-      href: "/servicios/marketing",
-      description:
-        "La mejor opción para publicidad y contenido en las redes sociales.",
-    },
-    {
-      title: "Capacitación empresarial",
-      href: "/servicios/capacitacion",
-      description: "Se la mejor versión de ti mismo que sabemos que puedes ser.",
-    },
-    {
-      title: "Estrategia y gestión empresarial",
-      href: "/servicios/estrategia",
-      description:
-        "Planeamos tu camino hacia un exito asegurado.",
-    },
-    {
-      title: "¿Buscas algo mas?",
-      href: "https://www.google.com.mx/maps/place/Agencia+Valore/@21.9072379,-102.2804263,17z/data=!3m1!4b1!4m6!3m5!1s0x8429ef73f5afeb9f:0x4aa07056238f3e51!8m2!3d21.9072329!4d-102.2778514!16s%2Fg%2F11tfh7nphr?entry=ttu",
-      description:
-        "Tengamos una cita, ¿te parece bien?.",
-    },
-  ]
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Imprenta",
+    href: "/servicios/imprenta",
+    description: "Opciónes que van desde Gran formato a impresión y diseño.",
+  },
+  {
+    title: "Branding Corporativo",
+    href: "/servicios/branding",
+    description: "Alcanza tus metas con nosotros, mejora tu imagen publicitaria.",
+  },
+  {
+    title: "Marketing digital",
+    href: "/servicios/marketing",
+    description: "La mejor opción para publicidad y contenido en las redes sociales.",
+  },
+  {
+    title: "Capacitación empresarial",
+    href: "/servicios/capacitacion",
+    description: "Se la mejor versión de ti mismo que sabemos que puedes ser.",
+  },
+  {
+    title: "Estrategia y gestión empresarial",
+    href: "/servicios/estrategia",
+    description: "Planeamos tu camino hacia un exito asegurado.",
+  },
+  {
+    title: "¿Buscas algo mas?",
+    href: "https://www.google.com.mx/maps/place/Agencia+Valore/@21.9072379,-102.2804263,17z/data=!3m1!4b1!4m6!3m5!1s0x8429ef73f5afeb9f:0x4aa07056238f3e51!8m2!3d21.9072329!4d-102.2778514!16s%2Fg%2F11tfh7nphr?entry=ttu",
+    description: "Tengamos una cita, ¿te parece bien?.",
+  },
+]
 
-  export function NavigationMenuDemo() {
-  
-    return(
-        
-  <NavigationMenu>
-  <NavigationMenuList>
-    <NavigationMenuItem>
-      <NavigationMenuTrigger
-      className="bg-cyan-600 text-white hover:bg-cyan-600 transition-all duration-300 text-lg mt-1"
-      >Nuestros servicios</NavigationMenuTrigger>
-      <NavigationMenuContent className="">
-      <ul className="bg-gray-200 grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+export function NavigationMenuDemo() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger
+            className="bg-cyan-600 text-white hover:bg-cyan-600 transition-all duration-300 text-lg mt-1"
+          >
+            Nuestros servicios
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="">
+            <ul className="bg-gray-200 grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
-                <div className="">
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                
-                  {component.description}
-                  
-                </ListItem>
+                <div key={component.title}>
+                  <ListItem
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
                 </div>
-              ))} 
+              ))}
             </ul>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  </NavigationMenuList>
-</NavigationMenu>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
 
-    )
-  }
-
- 
-  const ListItem = React.forwardRef<
+const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {

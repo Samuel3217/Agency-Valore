37ed1c44e771dd5405053b12b9f154a851245bb2
Samuel3217 <deriva335@hotmail.com>
2024-10-25 10:43:03 -0600

    UX updates target blank

--- src/app/Menu2.tsx
+++ src/app/Menu2.tsx
@@ -6,13 +6,13 @@ import {
   } from "@/components/ui/hover-card"
 import Link from "next/link"
 import Image from "next/image"
-  
+
 export function Menu2() {
   return (
     <div className="fixed bottom-5 right-5  flex round">
     <HoverCard>
       <HoverCardTrigger asChild>
-        <Image 
+        <Image
         src="/images/telefono.png"
         alt="Contactanos"
         height={50}
@@ -22,7 +22,7 @@ export function Menu2() {
       </HoverCardTrigger>
       <HoverCardContent className="w-80">
         <div className="flex justify-between space-x-4">
-         
+
           <div className="space-y-1">
             <h4 className="text-1xl font-semibold text-center">Valore</h4>
             <p className="text-sm border-b border-gray-300 w-[300px] text-center">
@@ -31,7 +31,7 @@ export function Menu2() {
             <div className="items-center pt-2">
             <ul className="list-none pt-2">
                                 <li className="flex items-center hover:text-blue-600 transition-all duration-300 hover:opacity-100 opacity-80">
-                                
+
                                   <Image src="/images/facebook.png"
                                   alt="facebook"
                                   width={30}
@@ -39,12 +39,13 @@ export function Menu2() {
                                   className="hover:opacity-80 rounded-full mr-2"
                                   ></Image>
                                     <Link
-                                        href="https://www.facebook.com/rrpp.publicidad"
+                                        href="https://www.facebook.com/rrpp.publicidad" passHref
                                         className="text-sm block"
+                                       target="_blank" rel="noopener noreferrer"
                                     >
                                         Facebook
                                     </Link>
-                                  
+
                                 </li>
                                 <br></br>
                                 <li className="flex items-center hover:text-green-600 transition-all duration-300 hover:opacity-100 opacity-80">
@@ -57,8 +58,9 @@ export function Menu2() {
                                   height={20}
                                   ></Image>
                                     <Link
-                                        href="https://wa.me/5214499800425"
+                                        href="https://wa.me/5214499800425" passHref
                                         className="text-sm block"
+                                        target="_blank" rel="noopener noreferrer"
                                     >
                                         WhatsApp
                                     </Link>
@@ -71,11 +73,12 @@ export function Menu2() {
                                   width={30}
                                   height={20}
                                   >
-                                    
+
                                   </Image>
                                     <Link
-                                    href="mailto:agencia.valore@gmail.com"
+                                    href="mailto:agencia.valore@gmail.com" passHref
                                     className="text-sm block"
+                                    target="_blank" rel="noopener noreferrer"
                                     >Gmail
                                     </Link>
                                 </li>
@@ -88,8 +91,9 @@ export function Menu2() {
                                   height={20}
                                   ></Image>
                                   <Link
-                                  href="https://www.instagram.com/agencia_valore/"
+                                  href="https://www.instagram.com/agencia_valore/" passHref
                                   className="text-sm block"
+                                  target="_blank" rel="noopener noreferrer"
                                   >Instagram
                                   </Link>
                                 </li>
